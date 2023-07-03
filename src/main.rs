47944fe6de27dd9235e535082f48c7bda9f77fb8
Mastagoon <inzilagiway@gmail.com>
2023-07-03 15:20:39 +0300

    break

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use std::{fs, io::BufRead};
+
 fn main() {
     println!("Hello, world!");
+    for line in fs::read_to_string("input.txt").unwrap().lines() {
+        let length = line.len();
+        let (first_compartment, second_compartment) = line.split_at(length / 2);
+        first_compartment.chars().into_iter().find(|c| {
+            if second_compartment.contains(c) {
+                return c;
+            }
+        });
+        // let middle = length/2;
+        // let splitter = line[5];
+        // let [] = line.
+    }
 }
